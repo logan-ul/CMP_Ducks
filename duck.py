ddd4105d25bd2302fdb7e55237cd68bcfd110035
logan-ul <loganul2017@gmail.com>
2026-03-09 09:53:55 -0400

    New update api changes

--- duck.py
+++ duck.py
@@ -7,7 +7,7 @@ class DuckManager:
         # This initializes the data object containing all the duck information
         # if internet is not available or for some reason the api is down it defaults to a local copy
         try:
-            data = requests.get("https://duckland-production.up.railway.app/ducks").json()
+            data = requests.get("https://api.ducks.ects-cmp.com/ducks").json()
             with open("cache.json", "w") as file:
                 json.dump(data, file, indent=4)
         except:
@@ -61,10 +61,10 @@ class Duck:
 
         # Body fields
         self.head_color = data["body"]["head"]
-        self.front1_color = data["body"]["front1"]
-        self.front2_color = data["body"]["front2"]
-        self.back1_color = data["body"]["back1"]
-        self.back2_color = data["body"]["back2"]
+        self.front_left_color = data["body"]["frontLeft"]
+        self.front_right_color = data["body"]["frontRight"]
+        self.rear_left_color = data["body"]["rearLeft"]
+        self.rear_right_color = data["body"]["rearRight"]
 
         # Stats fields
         self.strength = data["stats"]["strength"]
@@ -75,6 +75,37 @@ class Duck:
 
     def __str__(self):
         return f"{self.name.title()}, owned by {self.assembler.title()}"
+    
+    def update_data(self):
+        # Main fields
+        self.raw_data["_id"] = self.id
+        self.raw_data["name"] = self.name
+        self.raw_data["assember"] = self.assembler
+        self.raw_data["adjectives"] = self.adjectives
+        self.raw_data["derpy"] = self.derpy
+        self.raw_data["bio"] = self.bio
+        self.raw_data["date"] = self.date
+        self.raw_data["approved"] = self.approved
+        self.raw_data["__v"] = self.version
+
+        # Body fields
+        self.raw_data["body"]["head"] = self.head_color
+        self.raw_data["body"]["front1"] = self.front_left_color
+        self.raw_data["body"]["front2"] = self.front_right_color
+        self.raw_data["body"]["back1"] = self.rear_left_color
+        self.raw_data["body"]["back2"] = self.rear_right_color
+
+        # Stats fields
+        self.raw_data["stats"]["strength"] = self.strength
+        self.raw_data["stats"]["health"] = self.health
+        self.raw_data["stats"]["focus"] = self.focus
+        self.raw_data["stats"]["intelligence"] = self.intelligence
+        self.raw_data["stats"]["kindness"] = self.kindness
+
+        return self.raw_data
+    
+    def update_online_duck(self):
+        print(requests.patch(f"https://api.ducks.ects-cmp.com/ducks/{self.id}", self.update_data()))
 
 
 
@@ -84,6 +115,10 @@ if __name__ == "__main__":
     manager = DuckManager()
     for duck in manager.create_duck_list():
         print(duck)
-    print(manager.get_duck_by_id("69a881b504f2a69eea6979cf"))
-    print(manager.get_ducks_by_name("TEST")[0])
-    print(manager.get_ducks_by_name("Logan"))
+    duck1 = manager.get_duck_by_id("69a8ea5053e250fdaf139d6f")
+    print(duck1.raw_data)
+    duck1.derpy = True
+    print(duck1.raw_data)
+    print(duck1.update_data())
+    duck1.update_online_duck()
+
